7dc0b1a02f8b4ab4156d4f3c01818451739672fa
PM <100664895+OhItsPM@users.noreply.github.com>
2022-03-22 11:13:50 -0700

    Changed smart contract to go away from Dapp

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,28 +1,19 @@
 const { expect } = require("chai");
-const { constants } = require("ethers");
 const { ethers } = require("hardhat");
 
-describe("SoulBulbs", function () {
-  it("Should mint and transfer an NFT to someone", async function () {
-    const SoulBulbs = await ethers.getContractFactory("SoulBulbs");
-    const soulBulbs = await SoulBulbs.deploy();
-    await soulBulbs.deployed();
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
 
-    const recipient = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc";
-    const metadataURI = "cid/SoulBulbs001.png";
+    expect(await greeter.greet()).to.equal("Hello, world!");
 
-    let balance = await soulBulbs.balanceOf(recipient);
-    expect(balance).to.equal(0);
-    
-    const newlyMintedToken = await soulBulbs.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther("0.05") });
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-     
-    //wait until the transaction is minted
-    await newlyMintedToken.wait();
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
 
-    balance = await soulBulbs.balanceOf(recipient);
-    expect(balance).to.equal(1);
-
-    expect (await soulBulbs.isContentOwned(metadataURI)).to.equal(true);
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
